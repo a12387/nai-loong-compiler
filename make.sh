70e0d4ff740865f9862114057e030d93addbb384
a12387 <1364219910@qq.com>
2024-10-28 18:29:33 +0000

    fix2

--- make.sh
+++
@@ -1,7 +0,0 @@
-rm -r build
-rm hello.koopa
-rm hello.o
-cmake -DCMAKE_BUILD_TYPE=Debug -B build
-cmake --build build
-#./build/compiler -riscv hello.c -o hello.o
-./build/compiler -koopa hello.c -o hello.koopa
